23cc0a20fb2e021af2a57886c24ab39c348ad87b
William Mabia <mabiawilliam@gmail.com>
2021-07-27 04:48:26 -0400

    some changes I made to the design. I'm using the branch incase we don't want some stuff

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import './App.css';
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import Header from './Header';
-import Login from './Login.js';
-import { auth } from './firebase';
-import { useStateValue } from './StateProvider';
-import HeaderLoggedOut from './HeaderLoggedOut';
-import Register from './Register';
-import Seller from './Seller';
-import SidebarWindow from './SidebarWindow';
-import SidebarWithChat from './SidebarWithChat';
+import "./App.css";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
+import Login from "./Login.js";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import HeaderLoggedOut from "./HeaderLoggedOut";
+import Register from "./Register";
+import Seller from "./Seller";
+import SidebarWindow from "./SidebarWindow";
+import SidebarWithChat from "./SidebarWithChat";
 function App() {
   const [{ user }, dispatch] = useStateValue();
   useEffect(() => {
-    auth.onAuthStateChanged(authUser => {
-      if (authUser){
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
         // the user just logged in / the user was logged in
         dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
         // the user is logged out
         dispatch({
-          type: 'SET_USER',
-          user: null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <div className="App">
       <Router>
         <Switch>
-          
           <Route path="/register">
             <Register />
           </Route>
@@ -43,7 +42,7 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
-          
+
           {!user ? (
             <Route path="/messages/:roomId">
               <Login />
@@ -84,15 +83,15 @@ function App() {
           {!user ? (
             <Route path="/">
               <div className="home__background">
-                <img src="/Images/Home/image01.jpg" alt = "" />
-                <HeaderLoggedOut/>
+                <img src="/Images/Home/image01.jpg" alt="" />
+                <HeaderLoggedOut />
                 <Home />
               </div>
             </Route>
           ) : (
             <Route path="/">
               <div className="home__background">
-                <img src="/Images/Home/image01.jpg" alt = "" />
+                <img src="/Images/Home/image01.jpg" alt="" />
                 <Header />
                 <Home />
               </div>
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 import "./Home.css";
-import { Button } from '@material-ui/core';
+import { Button } from "@material-ui/core";
 
 function Home() {
-    return (
-        <div className="Home">
-            <div className="bot">
-                <img src="/Images/Home/bot_Transperent.gif" alt = ""/>
-            </div>
-            <div className="bot__container">
-                <h2>Hello Vaibhav, <br/> What Are You Looking To-Do Today?</h2>
-                <div className="buttonContainer">
-                    <Button variant="contained" type='primary'>Browse Listing</Button>
-                    <Button variant="contained" type='primary'>Post Listing</Button>
-                </div>
-            </div>
+  return (
+    <div className="Home">
+      <div className="bot"></div>
+      <div className="bot__container">
+        <h2>Housing Marketplace for Kitchener Waterloo Region</h2>
+        <div className="buttonContainer">
+          <Button variant="contained" type="primary">
+            Browse Listing
+          </Button>
+          <Button variant="contained" type="primary">
+            Post Listing
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Home;
