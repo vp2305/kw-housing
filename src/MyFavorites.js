81bb6469f7fc57b7032db598b1b2421440d70b70
dharig22 <dharigandhi@gmail.com>
2021-08-09 03:35:46 -0400

    added gifs
    gif add karyu

--- src/MyFavorites.js
+++ src/MyFavorites.js
@@ -53,30 +53,14 @@ function MyFavorites() {
             </div>
         ):(
             <div className="myPosting">
+                <h2>My Favorites</h2>
+                <div className="picture__wrapper03">
+                    <img src="/Images/Seller/myfavouritespic.gif" alt = "" />
+                </div>
                 <div className = "myPosting__wrapper">
-                    <h2>My Favorites</h2>
                     <hr/>
                     <div className = "post__wrapper">
                         {myFavorites.map((post) => (
-                            // <div className = "post__container">
-                            //     <div className = "post__leftContainer">
-                            //         <img src = {post.data.imageURLS[0]} alt = "" className="post__image"/>
-                            //     </div>
-                            //     <div className = "post__rightContainer">
-                            //         <h2>{post.data.address}</h2>
-                            //         <h4>${post.data.pricePerMonth} / Month</h4>
-                            //         <span className = "post__bedroom__washroom">
-                            //             <img src="https://img.icons8.com/ios-glyphs/30/000000/bedroom.png" alt = "bedroom icon" className="post__imageIcon" />
-                            //             <h4>Bedrooms: {post.data.bedrooms}</h4>
-                            //             <img id = "washrooms" src="https://img.icons8.com/material-sharp/30/000000/bath.png" alt = "bath icon" className="post__imageIcon"/>
-                            //             <h4 >Washrooms: {post.data.bathrooms}</h4>
-                            //         </span>
-                            //         <p>{post.data.description}</p>
-                            //         <div className = "button__container">
-                            //             <Button variant="contained" type="primary" onClick={()=> viewListing(post.id)}>View</Button>
-                            //         </div>
-                            //     </div>
-                            // </div>
                             <div className = "post__container">
                                 <InfoProperty 
                                     property = {post}
